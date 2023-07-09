0d9612c58afbdd1b6b85b41f7eb5241cc201c3a5
Samusarand <sandrazuniga.dg@gmail.com>
2023-07-09 00:44:49 -0400

    desplegando

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,9 @@
-#/usr on error
+#!/usr/bin/env bash
+#exit on error
 set -o errexit
 
 bundle install
-bundle exect rake assets:precompile
-bundle exect rake assets:clean
-bundle exect rake db:migrate
-bundle exect rake db:seed
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seeds
